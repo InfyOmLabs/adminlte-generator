884e140ed6a2d36b369db4d49046408ffa078563
Mitul Golakya <mtl.golakiya@gmail.com>
2016-06-14 10:05:29 -0400

    Applied fixes from StyleCI

--- tests/ApiTestTrait.php
+++ tests/ApiTestTrait.php
@@ -2,7 +2,7 @@
 
 trait ApiTestTrait
 {
-    public function assertApiResponse(Array $actualData)
+    public function assertApiResponse(array $actualData)
     {
         $this->assertApiSuccess();
 
@@ -19,7 +19,7 @@ trait ApiTestTrait
         $this->seeJson(['success' => true]);
     }
 
-    public function assertModelData(Array $actualData, Array $expectedData)
+    public function assertModelData(array $actualData, array $expectedData)
     {
         foreach ($actualData as $key => $value) {
             $this->assertEquals($actualData[$key], $expectedData[$key]);
