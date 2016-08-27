60208c4b788403e15a0bcf56fc700a28226a21d4
Mitul Golakya <mtl.golakiya@gmail.com>
2016-08-27 23:41:26 +0530

    minor fix

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/login';
+    protected $redirectTo = '/home';
 
     /**
      * Create a new authentication controller instance.
