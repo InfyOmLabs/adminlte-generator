ad5dfc34c89f06baf5b53e1483adc0f2f4091f9e
Mitul Golakiaya <mtl.golakiya@gmail.com>
2016-10-11 15:06:56 +0530

    updated to latest laravel-generator version

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,3 +30,15 @@ Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
 Route::post('password/reset', 'Auth\PasswordController@postReset');
 
 Route::get('/home', 'HomeController@index');
+
+/*
+|--------------------------------------------------------------------------
+| API routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['prefix' => 'api', 'namespace' => 'API'], function () {
+    Route::group(['prefix' => 'v1'], function () {
+        require config('infyom.laravel_generator.path.api_routes');
+    });
+});
---
+++ resources/views/layouts/datatables_css.blade.php
@@ -0,0 +1,4 @@
+<!-- DataTable Bootstrap -->
+<link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css">
