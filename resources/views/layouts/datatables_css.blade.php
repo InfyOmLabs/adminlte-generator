5d3965605d2399a70d6050bf23daab8784893cd8
Shailesh <shailesh.infyom@gmail.com>
2019-03-19 18:34:39 +0530

    fix(pkg): Package updated - 5.8 (#38)

--- resources/views/layouts/datatables_css.blade.php
+++ resources/views/layouts/datatables_css.blade.php
@@ -1,4 +1,3 @@
 <!-- DataTable Bootstrap -->
-<link href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="//cdn.datatables.net/1.10.12/css/jquery.dataTables.min.css">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.2.2/css/buttons.bootstrap.min.css">
+<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.3.1/css/buttons.bootstrap.min.css">
