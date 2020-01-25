5b4f7a074d86442b37a5660127dc463163ab688f
Vishal Ribdiya <33887755+vishalinfyom@users.noreply.github.com>
2020-01-24 23:33:36 -0800

    refactor: copyright year changed (#61)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -98,7 +98,7 @@
 
         <!-- Main Footer -->
         <footer class="main-footer" style="max-height: 100px;text-align: center">
-            <strong>Copyright © 2016 <a href="#">Company</a>.</strong> All rights reserved.
+            <strong>Copyright © {{date('Y')}} <a href="#">Company</a>.</strong> All rights reserved.
         </footer>
 
     </div>
