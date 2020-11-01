12336bcdb0b10e22e7783dd1b4b6bdf8ec96edb2
Berteh <36950+berteh@users.noreply.github.com>
2020-11-01 20:25:11 +0100

    CRUD lang strings

---
+++ resources/lang/en/crud.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | CRUD Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used in admin pages for CRUD
+    | operations. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'add_new' => 'New',
+    'action' => 'Action',
+    'cancel' => 'Cancel',
+    'save' => 'Save',
+
+];
