e6bc4ba325fdb5e0cf264bb51d465019d54ea23d
shailesh_infyom <shailesh.infyom@gmail.com>
2021-01-23 16:40:11 +0530

    refactor: remove old version

--- resources/views/layouts/datatables_css.blade.php
+++
@@ -1,3 +0,0 @@
-<!-- DataTable Bootstrap -->
-<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.3.1/css/buttons.bootstrap.min.css">
