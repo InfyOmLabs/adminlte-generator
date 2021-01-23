554eae83eb3776c36c363e3f0cecd4bba1fbb689
shailesh_infyom <shailesh.infyom@gmail.com>
2021-01-23 16:05:45 +0530

    remove old version

--- resources/views/layouts/datatables_js.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Datatables -->
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.3.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.3.1/js/buttons.colVis.min.js"></script>
-<script src="{{ asset('vendor/datatables/buttons.server-side.js') }}"></script>
